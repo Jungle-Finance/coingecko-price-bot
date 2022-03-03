6e2d6dde547d9b650fe10c3db136e2441e8b70ed
Chase Whipple <chasewhip20@gmail.com>
2022-03-03 13:17:11 -0700

    Make express listen on 0.0.0.0:80

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
  
 app.get('/', (req, res) => res.send('Hello World!'));
  
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Example app listening at http://0.0.0.0:${port}`));
 
 const { Client } = require('discord.js')
 const dotenv = require('dotenv')
