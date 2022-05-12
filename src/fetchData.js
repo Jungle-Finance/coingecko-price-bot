da548e9b3e45fbef5f37328871c2ebdd34a536be
Chase Whipple <chasewhip20@gmail.com>
2022-05-11 21:29:18 -0600

    Move to our api

--- src/fetchData.js
+++ src/fetchData.js
@@ -5,11 +5,11 @@ dotenv.config()
 
 exports.fetchData = async () => {
   try {
-    const tokenData = await (await fetch(`https://api.coingecko.com/api/v3/coins/${process.env.TOKEN_ID}`)).json()
+    const tokenData = await (await fetch(`https://api.jungledefi.io/v1/token/${process.env.TOKEN_ID}`)).json()
 
-    const price = tokenData.market_data.current_price.usd
-    const symbol = tokenData.symbol.toUpperCase()
-    const circSupply = tokenData.market_data.circulating_supply
+    const price = tokenData.data.price
+    const symbol = process.env.TOKEN_ID.toUpperCase()
+    const circSupply = tokenData.data.circulatingSupply
 
     return { price, symbol, circSupply }
   } catch (err) {
--- src/index.js
+++ src/index.js
@@ -36,6 +36,6 @@ client.setInterval(async () => {
     `MC: $${numberWithCommas(Math.round(price * circSupply))}`,
     { type: 'WATCHING' },
   )
-}, 1 * 60 * 1000)
+}, 2 * 1000)
 
 client.login(process.env.DISCORD_API_TOKEN)
